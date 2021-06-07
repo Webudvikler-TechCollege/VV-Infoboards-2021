345cc2e813af5624a343689e06710874399697dc
Niklas0010 <nlyberg@gmail.com>
2021-06-07 12:21:04 +0200

    datetime

---
+++ src/Components/Date/customDate.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import Style from './Date.module.scss'
+
+export const GetCurrentDate = () => {
+  const [date, setDate] = useState(new window.Date());
+
+  useEffect(() => {
+    setInterval(() => {
+      setDate(new window.Date());
+    }, 1000);
+  }, []);
+
+
+
+  return (
+    <>
+      <div className={Style.date}>
+        {date && date.toLocaleDateString([], {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          })}
+      </div>
+      <div className={Style.time}>
+        {date && date.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            }).replace(".", date.getSeconds() % 2 === 0 ? ":" : " ")}
+      </div>
+    </>
+  );
+};
