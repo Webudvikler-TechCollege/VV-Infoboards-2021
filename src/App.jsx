04d17c80c3324f94efb6c585669dbb21c7a90aa3
LolerSwager <53053540+LolerSwager@users.noreply.github.com>
2021-06-21 13:41:11 +0200

    news

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Activities from './Components/Activities/Activities';
 import { GetCurrentDate } from './Components/Date/customDate';
 import Menu from './Components/Menu/Menu';
-import { News } from './Components/news/News';
+import { News } from './Components/News/News';
 import { Bus } from './Components/Bus/Bus'
 // import Weather from './Components/Weather/Weather'
 import Img from './image/lol.png'
