83fa24de02588f95e57faa66a0b6b4e9c6810584
Heinz K <heka@techcollege.dk>
2021-09-09 09:57:29 +0200

    Spinner Update

--- src/Components/Activities/Activities.jsx
+++ src/Components/Activities/Activities.jsx
@@ -190,7 +190,7 @@ const Activities = () => {
                         <p>Afventer data...</p>
                         <div className={Style.spinner}>
                             <Loader
-                                type="Rings"                                
+                                type="Bars"                                
                                 color="#ddd"
                                 height={`50%`}
                                 width={`50%`}
